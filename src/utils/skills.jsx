fd7e4c5e2f5da062f5644e95d7d548276e8d6512
Gani Ramadhan <ganiramadhan22@gmail.com>
2024-07-06 22:02:46 +0700

    chore: update skills

--- src/utils/skills.jsx
+++ src/utils/skills.jsx
@@ -5,6 +5,9 @@ import laravelImage from '../assets/laravel.png';
 import javascriptImage from '../assets/javascript.jpg';
 import phpImage from '../assets/php.jpg';
 import postgreSqlImage from '../assets/postgresql.png';
+import bootstrapImage from '../assets/boostrap.png';
+import jqueryImage from '../assets/jquery.png';
+import tailwindcssImage from '../assets/tailwindcss.png';
 
 export const skills = [
     { title: 'Laravel', imageSrc: laravelImage, shortDescription: 'PHP framework for web applications.', link: 'https://laravel.com/' },
@@ -12,7 +15,9 @@ export const skills = [
     { title: 'React', imageSrc: reactImage, shortDescription: 'JavaScript library for building UIs.', link: 'https://reactjs.org/' },
     { title: 'Next.js', imageSrc: nextImage, shortDescription: 'Framework for server-side apps.', link: 'https://nextjs.org/' },
     { title: 'Node.js', imageSrc: nodejsImage, shortDescription: 'JavaScript runtime on the server side.', link: 'https://nodejs.org/' },
-    { title: 'Postgre SQL', imageSrc: postgreSqlImage, shortDescription: 'Open-source relational database.', link: 'https://www.postgresql.org/' },
+    { title: 'PostgreSQL', imageSrc: postgreSqlImage, shortDescription: 'Open-source relational database.', link: 'https://www.postgresql.org/' },
     { title: 'PHP', imageSrc: phpImage, shortDescription: 'Server-side scripting language.', link: 'https://www.php.net/' },
+    { title: 'Bootstrap', imageSrc: bootstrapImage, shortDescription: 'CSS framework for responsive design.', link: 'https://getbootstrap.com/' },
+    { title: 'jQuery', imageSrc: jqueryImage, shortDescription: 'JavaScript library for DOM manipulation.', link: 'https://jquery.com/' },
+    { title: 'Tailwind CSS', imageSrc: tailwindcssImage, shortDescription: 'Utility-first CSS framework.', link: 'https://tailwindcss.com/' },
 ];
-
